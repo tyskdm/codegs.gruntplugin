16ecf694a1444615471f2acbfb94ccf37f73f019
Tsuyoshi Kodama <tsuyoshi.kodama@gmail.com>
2014-04-14 17:40:39 +0900

    add test/case_01.

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,43 +16,60 @@ module.exports = function(grunt) {
       all: [
         'Gruntfile.js',
         'tasks/*.js',
-        '<%= nodeunit.tests %>',
+        '<%= nodeunit.tests %>'
       ],
       options: {
-        jshintrc: '.jshintrc',
-      },
+        jshintrc: '.jshintrc'
+      }
     },
 
     // Before generating any new files, remove any previously-created files.
     clean: {
-      tests: ['tmp'],
+      tests: ['tmp']
     },
 
     // Configuration to be run (and then tested).
     codegs: {
-      default_options: {
+      case_01: {
         options: {
+          packagefile: null,
+          rootdir: null,
+          mainfile: null,
+          core: null,
+          node_core: null,
+          node_modules: null,
+          kernel: null
         },
         files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!',
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
+          'tmp/case_01.js': [
+              'test/case_01/main.js',
+              'test/case_01/sub.js'
+          ]
+        }
+      }
+      //case_01: {
+      //  options: {
+      //    packagefile: 'test/case_01/package.json',
+      //    rootdir: '',
+      //    mainfile: '',
+      //    core: '',
+      //    node_core: '',
+      //    node_modules: [],
+      //    kernel: ''
+      //  },
+      //  files: {
+      //    'tmp/case_01.js': [
+      //        'test/case_01/testing',
+      //        'test/case_01/123'
+      //    ]
+      //  }
+      //}
     },
 
     // Unit tests.
     nodeunit: {
-      tests: ['test/*_test.js'],
-    },
-
+      tests: ['test/*_test.js']
+    }
   });
 
   // Actually load this plugin's task(s).
--- test/codegs_test.js
+++ test/codegs_test.js
@@ -27,22 +27,36 @@ exports.codegs = {
     // setup here if necessary
     done();
   },
-  default_options: function(test) {
+  case_01: function(test) {
     test.expect(1);
+    // console.log('');
 
-    var actual = grunt.file.read('tmp/default_options');
-    var expected = grunt.file.read('test/expected/default_options');
-    test.equal(actual, expected, 'should describe what the default behavior is.');
+    var actual = grunt.file.read('tmp/case_01.js').match(/^require\('module'\)\.(define|runmain)\('(.+)'(,|\);)$/gm);
+    // console.log(actual);
+
+    var expected = grunt.file.read('test/case_01/expected').split('\n');
+    // console.log(expected);
+
+    test.deepEqual(actual, expected, 'should describe what the custom option(s) behavior is.');
 
     test.done();
-  },
-  custom_options: function(test) {
-    test.expect(1);
-
-    var actual = grunt.file.read('tmp/custom_options');
-    var expected = grunt.file.read('test/expected/custom_options');
-    test.equal(actual, expected, 'should describe what the custom option(s) behavior is.');
-
-    test.done();
-  },
+  }
+  //default_options: function(test) {
+  //  test.expect(1);
+  //
+  //  var actual = grunt.file.read('tmp/default_options');
+  //  var expected = grunt.file.read('test/expected/default_options');
+  //  test.equal(actual, expected, 'should describe what the default behavior is.');
+  //
+  //  test.done();
+  //},
+  //custom_options: function(test) {
+  //  test.expect(1);
+  //
+  //  var actual = grunt.file.read('tmp/custom_options');
+  //  var expected = grunt.file.read('test/expected/custom_options');
+  //  test.equal(actual, expected, 'should describe what the custom option(s) behavior is.');
+  //
+  //  test.done();
+  //},
 };
